23d3c6d71708f86a9b3149d41abb798675a2014f
DIR\vincenzo.siciliano <vincenzo.siciliano@accenture.com>
2024-10-16 16:49:41 +0200

    Creato e ordinato progetto spring boot e aggiornato pom

---
+++ BatchLogAPI/src/test/java/prova/example/batchlogapi/BatchLogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package prova.example.batchlogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchLogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
